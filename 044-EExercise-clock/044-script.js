995b3b8b779ce2a9ca0619914bba3b544b48f67f
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-07-19 20:39:58 +0530

    1907

---
+++ 044-EExercise-clock/044-script.js
@@ -0,0 +1,9 @@
+let a = new Date()
+let h = a.getHours()
+let m = a.getMinutes()
+let s = a.getSeconds()
+let d = a.getDate()
+let y = a.getFullYear()
+let mo = a.getMonth()
+let w = a.getDay()
+console.log(h , m , s , d ,y , mo , w);
