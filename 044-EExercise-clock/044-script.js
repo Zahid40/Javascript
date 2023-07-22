37fb0a84640db2bed32ca5ef8a8bd904333a5289
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-07-22 11:46:05 +0530

    2207

--- 044-EExercise-clock/044-script.js
+++ 044-EExercise-clock/044-script.js
@@ -1,9 +1,21 @@
-let a = new Date()
-let h = a.getHours()
-let m = a.getMinutes()
-let s = a.getSeconds()
-let d = a.getDate()
-let y = a.getFullYear()
-let mo = a.getMonth()
-let w = a.getDay()
-console.log(h , m , s , d ,y , mo , w);
+
+setInterval(() => {
+    let a = new Date()
+    let h = a.getHours();
+    let m = a.getMinutes()*6;
+    let s = a.getSeconds()*6;
+    let d = a.getDate()
+    let y = a.getFullYear()
+    let mo = a.getMonth()
+    let w = a.getDay()
+    console.log("Hour = ",h,"Min = ", m,"sec = ", s,"date = ", d,"Year = ", y,"Month = ", mo,"Week = ", w);
+
+    let divsec = document.getElementById("sec");
+    divsec.style.transform = `rotate(${s}deg)`
+    let divmin = document.getElementById("min");
+    divmin.style.transform = `rotate(${m}deg)`
+    let divhour = document.getElementById("hour");
+    let hourhand = (h<=12)?h*30:(h-12)*30;
+    console.log(hourhand)
+    divhour.style.transform = `rotate(${hourhand + a.getMinutes()*.5}deg)`
+}, 1000);
---
+++ 046-Promises.js
@@ -0,0 +1,11 @@
+//solution of callback hell/pyramid of Doom
+
+let promise = new Promise(function(resolve,reject){
+    
+})
+
+console.log("Hello")
+setTimeout(function(){
+    console.log("Zahid")
+},2000)
+console.log("Hiiiiiii")
